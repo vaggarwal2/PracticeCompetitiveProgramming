951c3359e654a26e4b856fa853179db5be1342fe
Varsha Aggarwal <varsha_aggarwal@intuit.com>
2020-06-20 13:54:30 +0530

    remove duplicates in place

--- leetcode/ContainerWithMostWater.java
+++ leetcode/ContainerWithMostWater.java
@@ -35,7 +35,7 @@ public class ContainerWithMostWater {
 
     public static void main(String args[]) {
         Test test = new Test();
-        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height = {1,8,9,2,5,4,8};
         System.out.println(test.maxArea(height));
     }
 }
---
+++ leetcode/RemoveInPlaceDuplicate.java
@@ -0,0 +1,34 @@
+public class RemoveInPlaceDuplicate {
+
+    public static class Test {
+        public int  removeDuplicates(int[] nums) {
+            int i = 0, j = 1;
+            boolean swap = false;
+
+            while (j < nums.length) {
+                if (nums[i] != nums[j]) {
+                    i++;
+                    if (swap) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        // swap = false;
+                    }
+                    j++;
+                } else if (nums[i] == nums[j]) {
+                    swap = true;
+                    j++;
+                }
+            }
+            return i + 1;
+        }
+    }
+    public static void main(String args[]) {
+        Test test = new Test();
+        int[] a = new int[]{1,1,2,3};
+        System.out.println(test.removeDuplicates(a));
+        for ( int i = 0 ; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
